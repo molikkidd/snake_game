174fcab1ef86008cfaa81cbe28bdd1586d4109e2
Molikkidd <mzkidd5@gmail.com>
2022-12-15 12:49:41 -0500

    add canvas to application

--- src/main/java/com/example/snake_game/snake.java
+++ src/main/java/com/example/snake_game/snake.java
@@ -1,10 +1,15 @@
 package com.example.snake_game;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class snake extends Application {
     static int speed = 5;
@@ -16,6 +21,8 @@ public class snake extends Application {
     static int cornersize = 25;
     static List<Corner> snake = new ArrayList<>();
     static Dir direction = Dir.left;
+    static boolean gameOver = false;
+    static Random rand = new Random();
 
     public enum Dir{
         left,right,up,down
@@ -35,6 +42,29 @@ public class snake extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        try {
+            VBox root = new VBox();
+            Canvas c = new Canvas(width*cornersize,height*cornersize);
+            GraphicsContext gc = c.getGraphicsContext2D();
+            root.getChildren().add(c);
+            new AnimationTimer(){
+                long lastTick = 0;
 
+                public void handle (long now) {
+                    if(lastTick == 0) {
+                        lastTick = now;
+                        return;
+                    }
+
+                    if(now - lastTick > 1000000000/ speed) {
+                        lastTick = now;
+                    }
+                }
+            }.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+
 }
